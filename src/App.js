9d784d816d3edbfca1bbf0c8cdc8d5c469f5d6eb
iman <i.jalali88@gmail.com>
2020-08-01 12:55:49 +0430

    run VisibleTodolist and Footer component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import AddTodo from "./containers/AddTodo";
+import VisibleTodoList from './containers/VisibleTodoList';
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div>
       <AddTodo/>
+      <VisibleTodoList/>
+      <Footer/>
     </div>
   );
 }

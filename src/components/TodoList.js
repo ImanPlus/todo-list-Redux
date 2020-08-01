85a418c5605c0cdd653f1fc2cbdded1ef1691b05
iman <i.jalali88@gmail.com>
2020-08-01 09:38:35 +0430

    TodoList component

---
+++ src/components/TodoList.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Todo from "./Todo";
+import PropTypes from 'prop-types';
+
+const TodoList = ({todos, toggleTodo}) => (
+  <ul>
+    {
+      todos.map(todo => (
+        <Todo
+          key={todo.id}
+          {...todo}
+          onClick={() => toggleTodo(todo.id)}
+        />
+      ))
+    }
+  </ul>
+)
+
+TodoList.prototype = {
+  todos: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      completed: PropTypes.bool.isRequired,
+      text: PropTypes.string.isRequired,
+    }).isRequired
+  ).isRequired,
+  toggleTodo: PropTypes.func.isRequired
+}
